768c05a24783fba7f314e031a08c1f98eeed666b
Marvin Gravert <marvin.gravert@gmail.com>
2019-12-08 20:50:12 +0100

    Plotclass created, small bugs still left

--- plotTest.py
+++ plotTest.py
@@ -4,6 +4,21 @@ import numpy as np
 import time
 import random
 from matplotlib.animation import FuncAnimation
+from scipy.spatial.transform import Rotation as R 
+
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -13,10 +28,22 @@ def animate(i):
     ax.set_ylim3d(0,1)
     ax.set_zlim3d(0,1)
     x=getPoint()#returns a random matrix 1x3
-    return ax.scatter3D(x[0][0],x[0][1],x[0][2])
+    ax.scatter3D(x[0],x[1],x[2])
+    #ax.plot([x[0],1],[x[1],1],[x[2],1],'ro',alpha=0.8, lw=3)
+    a = Arrow3D([x[0], 1], [x[1], 1], 
+                [x[2],1], mutation_scale=20, 
+                lw=3, arrowstyle="-|>", color="r")
+    ax.add_artist(a)
+    return ax
 def getPoint():
-    return np.random.rand(1,3)
-ani = FuncAnimation(fig, animate,  interval=1000)
+    return np.random.rand(3)
+    userInput=input("Enter Coordinantes in format x y z")
+    userInput=userInput.split(" ")
+    userInput=[float(x) for x in userInput]
+    return userInput
+
+
+ani = FuncAnimation(fig, animate,  interval=10)
 #fig is the figure we plot onto
 #animate is the function who returns a fitting graph object "to add to fig"(the i indicates the frame number)
 #repeat, if the animation should show itself again at the end(repeat=False) standard is true
---
+++ plotting3DPose.py
@@ -0,0 +1,118 @@
+#plotting
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+
+#math modules
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+#vive interface modules
+from triad_openvr import triad_openvr
+
+#helper class to create arrowHeads
+#source:https://stackoverflow.com/questions/22867620/putting-arrowheads-on-vectors-in-matplotlibs-3d-plot
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
+class Plotting3DPose(object):
+    """
+    Creates 3D Plot object which can be used to plot 3D Pose data dynamicially (at given update rate)
+    """
+    baseXVec=[1,0,0]
+    baseYVec=[0,1,0]
+    baseZVec=[0,0,1]
+    def __init__(self,interval=1000,numPoses=1,boundary=[10,10,10]):
+        """
+        interval: min time between updates tothe graph
+        numPoses: number of poses that will be plotted(for now only one)
+        boundary:list of boundary of the area to be plotted[x,y,z]
+        """
+        self._interval=interval
+        self.numPoses=numPoses#for now just placeholder
+        self.fig= plt.figure()#figure to plot onto
+        self.ax=self.fig.add_subplot(111, projection='3d')#3d added to the figure
+        self.boundary=boundary
+        self.poseList=[]
+        for i in range(numPoses):
+            self.poseList.append([[0,0,0],[0,0,0,1]])#x y z w
+    @property
+    def interval(self):
+        return self._interval
+    @interval.setter
+    def interval(self,interval):
+        self._interval=interval
+
+    def start(self):
+        """
+        start the plotting
+        """
+        def _animate(i):
+            self.update()
+            
+            self.ax.cla()#clear previous graphs
+            #set boundary to not allow for 
+            self.ax.set_xlim3d(-self.boundary[0],self.boundary[0])
+            self.ax.set_ylim3d(-self.boundary[1],self.boundary[1])
+            self.ax.set_zlim3d(-self.boundary[2],self.boundary[2])
+            for pose in self.poseList:
+                #plot the origin of coordinate system
+                self.ax.plot([pose[0][0]],pose[0][1],pose[0][2],'ro',alpha=0.8, lw=3)
+                print([pose[0][0]],pose[0][1],pose[0][2])
+                #plot the axis of the system 
+                r=R.from_quat(pose[1])
+                xNew=r.apply(self.baseXVec)+pose[0]
+                yNew=r.apply(self.baseYVec)+pose[0]
+                zNew=r.apply(self.baseZVec)+pose[0]
+                print(xNew) 
+                print(xNew[1])             
+                #plot The axis x= blue y= yellow z=red
+                #if the axis need to be scaled, scale the base vectors at the top of this class implementation
+                xAxis = Arrow3D([[pose[0][0]], xNew[0]], [[pose[0][1]], xNew[1]], [[pose[0][2]],xNew[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="b")
+                yAxis = Arrow3D([[pose[0][0]], yNew[0]], [[pose[0][1]], yNew[1]], [[pose[0][2]],yNew[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="y")
+                zAxis = Arrow3D([[pose[0][0]], zNew[0]], [[pose[0][1]], zNew[1]], [[pose[0][2]],zNew[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
+                self.ax.add_artist(xAxis)
+                self.ax.add_artist(yAxis)
+                self.ax.add_artist(zAxis)
+            return self.ax
+        ani = FuncAnimation(self.fig, _animate,  self.interval)
+        plt.show()
+    
+    def update(self):
+        """
+        update the pose values
+        """
+        for i in range(self.numPoses):
+            userInput=input("enter here new Pose as translation and rotation(quaternion). Format x y z i j k w\n").split(" ")
+            #format into 2 lists of three and four entires 
+            self.poseList[i]=[[float(x) for x in userInput[0:3]],[float(x) for x in userInput[3:]]]
+    def updateQuaternion(self,newPose,objNumber):
+        """
+        updates the pose of one of the tracked objects
+        newPose: list of two lists, first containing translation pose [x y z] second roation
+            as quaternion [i j k w]
+        objNumber: which object's pose is supposed to be updated
+        """
+        self.poseList[objNumber]=newPose
+    def updateVive(self):
+        try:
+            [x,y,z,w, i, j, k]self.v.devices["controller_1"].get_pose_quaternion()
+        except TypeError :
+            pass
+    def initVive(self):
+        v = triad_openvr.triad_openvr()
+        self.v.print_discovered_objects()
+        
+test=Plotting3DPose(1000,1)
+test.start()
+
